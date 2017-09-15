57633cf4cc966d9838a9b7444fcb5db79534ea4a
allan-rehhoff-63 <allan.rehhoff@gmail.com>
2017-09-15 09:05:31 +0200

    Only let bundled autoloader load own library files

--- autoload.php
+++ autoload.php
@@ -1,17 +1,14 @@
 <?php
 	/**
-	* Load any required library files as-needed
+	* Load any required own library files as-needed.
 	* @author Allan Thue Rehhoff
 	* @version 2.0
 	* @package HttpRequest
 	* @license WTFPL
 	*/
-	if(!class_exists("HttpRequest")) {
-		spl_autoload_register(function($class) {
-			//$filename = __DIR__.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR.$class.".class.php";
-			$filename = __DIR__.DIRECTORY_SEPARATOR.str_replace("\\", DIRECTORY_SEPARATOR, $class).".class.php";
-			if(is_readable($filename) && !class_exists($class)) {
-				require $filename;
-			}
-		}, true, true);
-	}
+	spl_autoload_register(function($class) {
+		$filename = __DIR__.DIRECTORY_SEPARATOR.str_replace("\\", DIRECTORY_SEPARATOR, $class).".class.php";
+		if(is_readable($filename) && !class_exists($class) && substr($class, 0, 4) == "Http") {
+			require $filename;
+		}
+	}, true, true);
